15e4cb64a209279b49039ca91b5f5699c37f949b
Martin <martinagnalv@gmail.com>
2020-12-14 12:43:21 +0100

    Update IsPlayingPackage.java

--- android/src/main/java/com/reactlibrary/IsPlayingPackage.java
+++ android/src/main/java/com/reactlibrary/IsPlayingPackage.java
@@ -13,7 +13,7 @@ import com.facebook.react.bridge.JavaScriptModule;
 public class IsPlayingPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new isPlayingModule(reactContext));
+      return Arrays.<NativeModule>asList(new IsPlayingModule(reactContext));
     }
 
     // Deprecated from RN 0.47
